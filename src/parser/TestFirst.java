66e10b6b4867e2ef593293d91d43374e9842b0a6
Tom <Tom@172.16.2.35>
2012-01-11 18:50:03 +0200

    hghg

---
+++ src/parser/TestFirst.java
@@ -0,0 +1,13 @@
+package parser;
+
+import grammar.*;
+
+public class TestFirst {
+	public static void main(String[] args) throws Exception {
+		Grammar g = new Grammar();
+		g.loadFromFile("grammar2.txt");
+		
+		LLOneParser parser = new LLOneParser(g);
+		parser.printFirst(new Symbol("S"));
+	}
+}
